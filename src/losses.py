790b9ca2f47bb4d11d82899057d454921fbe06dd
Fereshteh Yousefirizi <84542058+qurit-frizi@users.noreply.github.com>
2022-08-05 15:55:20 -0700

    Add files via upload

---
+++ src/losses.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+from torch import nn
+import torch
+import math
+from torch.nn import functional as F
+import operator
+from functools import reduce
+import numpy as np
+
+
+import torch
+from torch import nn
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+        self.smooth = 1
+
+    def forward(self, input, target):
+        axes = tuple(range(1, input.dim()))
+        intersect = (input * target).sum(dim=axes)
+        union = torch.pow(input, 2).sum(dim=axes) + torch.pow(target, 2).sum(dim=axes)
+        loss = 1 - (2 * intersect + self.smooth) / (union + self.smooth)
+        return loss.mean()
+
+
---
+++ src/metrics.py
@@ -0,0 +1,10 @@
+def dice(input, target):
+    axes = tuple(range(1, input.dim()))
+    bin_input = (input > 0.5).float()
+
+    intersect = (bin_input * target).sum(dim=axes)
+    union = bin_input.sum(dim=axes) + target.sum(dim=axes)
+    score = 2 * intersect / (union + 1e-3)
+
+    return score.mean()
+
